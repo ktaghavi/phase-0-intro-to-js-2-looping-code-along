9f720ce9af4acf1827a61c2a16663311de61e803
Kamyar Taghavi <caracicatriz.kt@gmail.com>
2023-06-06 23:43:40 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+let cardMessages = [];
+
+function writeCards(names) {
+    for (let x = 0; x < names.length; x++) {
+        cardMessages.push(`Thank you, ${names[x]}, for the wonderful surprise gift!`);
+    };
+    return cardMessages;
+}
+
+function countDown(i) {
+    for (let i = 0; i < 11; i++) 
+        console.log(i);
+};
+
+countDown (4);
